2e8bfdc012b551e7840c7b31cb369ac75f769f0e
shafik720 <shafikrasel5@gmail.com>
2024-07-18 00:58:26 +0600

    redux error for typescript for state unknown type solved

--- src/app/(mainLayout)/page.tsx
+++ src/app/(mainLayout)/page.tsx
@@ -1,11 +1,10 @@
 "use client" ;
-import { goToPage } from "@/Redux/features/pageRouting/pageRoutingSlice";
+import { useAppDispatch, useAppSelector } from "@/Redux/store/reduxHooks";
 import Sidebar from "@/components/Sidebar/Sidebar";
-import { useDispatch, useSelector } from "react-redux";
 
 export default function Home() {
-  const dispatch = useDispatch() ;
-  const pageStatus = useSelector(state => state.pageRouting.currentPage) ;
+  const dispatch = useAppDispatch() ;
+  const pageStatus = useAppSelector(state => state.pageRouting.currentPage) ;
   console.log(pageStatus);
   return (
     <main className="flex  rounded container flex-col items-start justify-center p-10 shadow-2xl bg-white">
